6aac37766d5f20208b81d0fd7922cbf0a7ee690e
kr-aditya18 <vermaadiitya13678@gmail.com>
2026-02-14 23:14:44 +0530

    Selection Sort Done!!

---
+++ selectionSort.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+void selectionSort(int arr[],int n){
+    for(int i = 0; i<=n-1; i++){
+        int mini = i;
+        for(int j = i+1; j<=n-1;j++){
+            if(arr[j]<arr[mini]){
+                mini = j;
+            }
+        }
+        swap(arr[i],arr[mini]);
+    }
+}
+
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i = 0; i<n;i++) cin>>arr[i];
+    selectionSort(arr,n);
+    for(int i = 0; i<n; i++) cout<<arr[i]<<" ";
+    return 0;
+}
